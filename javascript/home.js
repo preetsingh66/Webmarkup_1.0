1e763218fa7ca8cc59f32984ae529a9419367980
preetsingh66 <115453789+preetsingh66@users.noreply.github.com>
2024-01-17 19:31:13 +0100

    made Responsive design and added some missing elements

---
+++ javascript/home.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const acceptCookiesBtn = document.querySelector('.accept-cookies');
+    const cookiesMessage = document.querySelector('.cookies-message');
+  
+    acceptCookiesBtn.addEventListener('click', function() {
+      cookiesMessage.style.display = 'none';
+  
+     
+    });
+  });
+  
+  
